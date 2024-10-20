4fc337ed4f0a7c3cb17e0efbb7c4e90b57e84f5a
LAPTOP-RM2FOVBS\ADMIN <nghiamtce170746@fpt.edu.vn>
2024-10-20 20:01:33 +0700

    CartItem

--- Users/LENOVO/OneDrive/Documents/NetBeansProjects/123/src/main/java/Controllers/CartItemController.java
+++ Users/LENOVO/OneDrive/Documents/NetBeansProjects/123/src/main/java/Controllers/CartItemController.java
@@ -8,12 +8,16 @@ import DAOs.ViewCategoryDAO;
 import DTOs.CartSummary;
 import Models.CartItem;
 import Models.Category;
+import Models.Option;
 import Models.Product;
+import Models.ProductOption;
+import Models.User;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,11 +26,9 @@ import java.util.List;
  * @author ASUS
  */
 public class CartItemController extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        // Display the cart
         GetCartItem(request, response);
     }
 
@@ -34,45 +36,67 @@ public class CartItemController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         String action = request.getParameter("action");
-
+        String action_update = request.getParameter("action_update_all");
         if ("add".equalsIgnoreCase(action)) {
             AddCartItem(request, response);
         } else if ("update".equalsIgnoreCase(action)) {
 //            UpdateCartItem(request, response);
         } else if ("delete".equalsIgnoreCase(action)) {
             DeleteCart(request, response);
+        } else if("update_all".equalsIgnoreCase(action_update)) {
+            UpdateCart(request, response);
         }
     }
     private void GetCartItem(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException{
-        int userId = 1; 
-        CartItemDAO cartItemDAO = new CartItemDAO();
-        
-        List<CartItem> cartItems = cartItemDAO.getCartItemsByUserId(userId);
-        
-        List<Product> products = new ArrayList<>();
-        
-        for (CartItem item : cartItems) {
-            Product product = cartItemDAO.getProductById(item.getProduct_id());
-            products.add(product); 
-        }
+        try{
+            HttpSession session = request.getSession();
+            User acc = (User) session.getAttribute("Users");
+            int uid = acc.getUser_id();
+            double originalPrice = 0.0;
+            double discount = 0.0;
+            double shippingFee = 20.0; 
+            CartItemDAO cartItemDAO = new CartItemDAO();
+            String errorParam = request.getParameter("error");
+            List<CartItem> cartItems = cartItemDAO.getCartItemsByUserId(uid);
+            List<Option> options= new ArrayList<>();
+            List<Product> products = new ArrayList<>();
+            List<Option> avalivableOption = new ArrayList<>();
+            for (CartItem item : cartItems) {
+                Product product = cartItemDAO.getProductById(item.getProduct_id());
+                products.add(product); 
+                originalPrice += product.getProduct_price() * item.getQuantity();
+                int productOptionId = item.getProduct_option_id();
+                if ( productOptionId != 0 ){
+                    Option option = cartItemDAO.getOptionsByProductOptionId(productOptionId);
+                    options.add(option);
+                }
+                else{
+                    Option op = new Option();
+                    options.add(op);
+                }
+            }
+            for (Product item : products) {
+                avalivableOption = cartItemDAO.getOptionsByProductId(item.getProduct_id());
+                item.setOptions(avalivableOption);
+            }
+            double total = originalPrice - discount + shippingFee;
+                        ViewCategoryDAO dao = new ViewCategoryDAO();
+            List<Category> categoryList = dao.getAllCategories();
+            if(errorParam != null){
+                request.setAttribute("error", errorParam);
+            }
+            request.setAttribute("categoryList", categoryList);
+            request.setAttribute("cartItems", cartItems);
+            request.setAttribute("products", products); 
+            request.setAttribute("allOptions", options);
+            request.setAttribute("cartSummary", new CartSummary(originalPrice, discount, shippingFee, total));
 
-        double originalPrice = 0.0;
-        double discount = 0.0;
-        double shippingFee = 20.0; 
-        for (CartItem item : cartItems) {
-            Product product = cartItemDAO.getProductById(item.getProduct_id());
-            originalPrice += product.getProduct_price() * item.getQuantity();
+            request.getRequestDispatcher("/CartItem.jsp").forward(request, response);
         }
-        double total = originalPrice - discount + shippingFee;
-                    ViewCategoryDAO dao = new ViewCategoryDAO();
-        List<Category> categoryList = dao.getAllCategories();
-        request.setAttribute("categoryList", categoryList);
-        request.setAttribute("cartItems", cartItems);
-        request.setAttribute("products", products); 
-        request.setAttribute("cartSummary", new CartSummary(originalPrice, discount, shippingFee, total));
-        
-        request.getRequestDispatcher("/CartItem.jsp").forward(request, response);
+        catch(Exception e){
+            response.sendRedirect("/login.jsp"); 
+        }
     }
     
     private void DeleteCart(HttpServletRequest request, HttpServletResponse response)
@@ -85,26 +109,58 @@ public class CartItemController extends HttpServlet {
     
     private void AddCartItem(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-//        int userId = Integer.parseInt(request.getParameter("userId"));
-//        int productId = Integer.parseInt(request.getParameter("productId"));
-//        int productOptionId = Integer.parseInt(request.getParameter("productOptionId"));
-//        int quantity = Integer.parseInt(request.getParameter("quantity"));
-        int userId = 1;
-        int productId = Integer.parseInt(request.getParameter("productId"));
-        int productOptionId = 1;
-        int quantity = 1;
-        CartItem cartItem = new CartItem(0, userId, productId, productOptionId, quantity);
-        CartItemDAO cartItemDAO = new CartItemDAO();
+        try{
+            CartItemDAO cartItemDAO = new CartItemDAO();
+            HttpSession session = request.getSession();
+            User acc = (User) session.getAttribute("Users");
+            int uid = acc.getUser_id();
+            int productId = Integer.parseInt(request.getParameter("productId"));
+            int optionId = Integer.parseInt(request.getParameter("productOptionId"));
+            int quantity = Integer.parseInt(request.getParameter("productQuantity"));
+            ProductOption temp = cartItemDAO.GetProOptionByOptionId(productId, optionId);
+            CartItem cartItem = null;
+            if(temp == null){
+                cartItem = new CartItem(0, uid, productId, 0, quantity);
+            }
+            else{
+                cartItem = new CartItem(0, uid, productId, temp.getProduct_option_id(), quantity);
+            }
+            boolean isAdded = cartItemDAO.addCartItem(cartItem);
 
-        boolean isAdded = cartItemDAO.addCartItem(cartItem);
-
-        if (isAdded) {
-            response.sendRedirect("Cart"); 
-        } else {
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to add item to cart.");
+            if (isAdded) {
+                response.sendRedirect("Cart"); 
+            } else {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to add item to cart.");
+            }
+        }
+        catch(Exception e){
+            response.sendRedirect("/login.jsp"); 
         }
     }
     
+    private void UpdateCart(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        int newOptionId = 0;
+        HttpSession session = request.getSession();
+        User acc = (User) session.getAttribute("Users");
+        int uid = acc.getUser_id();
+        CartItemDAO cartItemDAO = new CartItemDAO();
+        List<CartItem> cartItems = cartItemDAO.getCartItemsByUserId(uid);
+        for (CartItem cartItem : cartItems) {
+            int cartItemId = cartItem.getCart_item_id();
+            int productId = cartItem.getProduct_id();
+            String check = request.getParameter("option_" + cartItemId);
+            int newQuantity = Integer.parseInt(request.getParameter("quantity_" + cartItemId));
+            if(check != null){
+                newOptionId = Integer.parseInt(check);
+            }
+            else {
+                newOptionId = 0;
+            }
+            cartItemDAO.updateCartItem(cartItemId, productId, newQuantity, newOptionId);
+        }
+        response.sendRedirect("/Cart");
+    }
     @Override
     public String getServletInfo() {
         return "Short description";
--- Users/LENOVO/OneDrive/Documents/NetBeansProjects/123/src/main/java/DAOs/CartItemDAO.java
+++ Users/LENOVO/OneDrive/Documents/NetBeansProjects/123/src/main/java/DAOs/CartItemDAO.java
@@ -2,7 +2,9 @@ package DAOs;
 
 import DBConnection.DBConnection;
 import Models.CartItem;
+import Models.Option;
 import Models.Product;
+import Models.ProductOption;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,18 +15,72 @@ import java.util.List;
 public class CartItemDAO {
 
     public boolean addCartItem(CartItem cartItem) {
-        String query = "INSERT INTO CartItems (user_id, product_id, product_option_id, quantity) VALUES (?, ?, ?, ?)";
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
+        String checkQuery = "SELECT quantity FROM CartItems WHERE user_id = ? AND product_id = ? AND product_option_id = ?";
+        String updateQuery = "UPDATE CartItems SET quantity = quantity + ? WHERE user_id = ? AND product_id = ? AND product_option_id = ?";
+        String insertQuery = "INSERT INTO CartItems (user_id, product_id, product_option_id, quantity) VALUES (?, ?, ?, ?)";
+        if(cartItem.getProduct_option_id() == 0){
+            checkQuery = "SELECT quantity FROM CartItems WHERE user_id = ? AND product_id = ? AND product_option_id IS NULL";
+            updateQuery = "UPDATE CartItems SET quantity = quantity + ? WHERE user_id = ? AND product_id = ? AND product_option_id IS NULL";
+            insertQuery = "INSERT INTO CartItems (user_id, product_id, product_option_id, quantity) VALUES (?, ?, NULL, ?)";
+            try (Connection conn = DBConnection.getConnection();
+                 PreparedStatement checkStmt = conn.prepareStatement(checkQuery);
+                 PreparedStatement updateStmt = conn.prepareStatement(updateQuery);
+                 PreparedStatement insertStmt = conn.prepareStatement(insertQuery)) {
 
-            stmt.setInt(1, cartItem.getUser_id());
-            stmt.setInt(2, cartItem.getProduct_id());
-            stmt.setInt(3, cartItem.getProduct_option_id());
-            stmt.setInt(4, cartItem.getQuantity());
-            
-            return stmt.executeUpdate() > 0;
-        } catch (SQLException e) {
-            e.printStackTrace();
+                checkStmt.setInt(1, cartItem.getUser_id());
+                checkStmt.setInt(2, cartItem.getProduct_id());
+
+                ResultSet rs = checkStmt.executeQuery();
+
+                if (rs.next()) {
+                    int currentQuantity = rs.getInt("quantity");
+                    updateStmt.setInt(1, cartItem.getQuantity());
+                    updateStmt.setInt(2, cartItem.getUser_id());
+                    updateStmt.setInt(3, cartItem.getProduct_id());
+
+                    return updateStmt.executeUpdate() > 0; 
+                } else {
+                    insertStmt.setInt(1, cartItem.getUser_id());
+                    insertStmt.setInt(2, cartItem.getProduct_id());
+                    insertStmt.setInt(3, cartItem.getQuantity());
+
+                    return insertStmt.executeUpdate() > 0; 
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            try (Connection conn = DBConnection.getConnection();
+                 PreparedStatement checkStmt = conn.prepareStatement(checkQuery);
+                 PreparedStatement updateStmt = conn.prepareStatement(updateQuery);
+                 PreparedStatement insertStmt = conn.prepareStatement(insertQuery)) {
+
+                checkStmt.setInt(1, cartItem.getUser_id());
+                checkStmt.setInt(2, cartItem.getProduct_id());
+                checkStmt.setInt(3, cartItem.getProduct_option_id());
+
+                ResultSet rs = checkStmt.executeQuery();
+
+                if (rs.next()) {
+                    int currentQuantity = rs.getInt("quantity");
+                    updateStmt.setInt(1, cartItem.getQuantity());
+                    updateStmt.setInt(2, cartItem.getUser_id());
+                    updateStmt.setInt(3, cartItem.getProduct_id());
+                    updateStmt.setInt(4, cartItem.getProduct_option_id());
+
+                    return updateStmt.executeUpdate() > 0; 
+                } else {
+                    insertStmt.setInt(1, cartItem.getUser_id());
+                    insertStmt.setInt(2, cartItem.getProduct_id());
+                    insertStmt.setInt(3, cartItem.getProduct_option_id());
+                    insertStmt.setInt(4, cartItem.getQuantity());
+
+                    return insertStmt.executeUpdate() > 0; 
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         return false;
     }
@@ -87,7 +143,6 @@ public class CartItemDAO {
 
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
-                    // Tạo một đối tượng Product và gán giá trị từ ResultSet
                     product = new Product(
                         rs.getInt("product_id"),
                         rs.getString("product_name"),
@@ -131,6 +186,107 @@ public class CartItemDAO {
     }
     
     
+    
+    public ProductOption GetProOptionByOptionId(int productId, int optionId) {
+        String selectQuery = "SELECT * FROM ProductOptions WHERE product_id = ? AND option_id = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement selectStmt = conn.prepareStatement(selectQuery)) {
+
+            selectStmt.setInt(1, productId);
+            selectStmt.setInt(2, optionId);
+
+            ResultSet rs = selectStmt.executeQuery();
+
+            if (rs.next()) {
+                ProductOption productOption = new ProductOption();
+                productOption.setProduct_option_id(rs.getInt("product_option_id"));
+                productOption.setProduct_id(rs.getInt("product_id"));
+                productOption.setOption_id(rs.getInt("option_id"));
+                return productOption;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;  
+    }
+    
+    public Option getOptionsByProductOptionId(int productOptionId) {
+        Option options = new Option();
+        String sql = "SELECT o.option_id, o.option_name " +
+                     "FROM ProductOptions po " +
+                     "JOIN Options o ON po.option_id = o.option_id " +
+                     "WHERE po.product_option_id = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, productOptionId);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                options.setOption_id(rs.getInt("option_id"));
+                options.setOption_name(rs.getString("option_name"));
+                options.setPrice_adjustment(rs.getDouble("price_adjustment"));
+                options.setIs_hidden(rs.getInt("is_hidden"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return options;
+    }
+    
+    public void updateCartItem(int cartItemId, int productId, int newQuantity, int newOptionId) {
+        String sql = "UPDATE cartitems SET quantity = ?, product_option_id = (SELECT product_option_id FROM productoptions WHERE product_id = ? AND option_id = ?) WHERE cart_item_id = ?";
+        if (newOptionId == 0){
+            sql = "UPDATE cartitems SET quantity = ? WHERE cart_item_id = ?";
+            try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+                ps.setInt(1, newQuantity);     
+                ps.setInt(2, cartItemId);          
+                ps.executeUpdate();              
+            } catch (SQLException e) {
+                e.printStackTrace();              
+            }
+        }
+        else{
+            try (Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+               ps.setInt(1, newQuantity);     
+               ps.setInt(2, productId);        
+               ps.setInt(3, newOptionId);       
+               ps.setInt(4, cartItemId);        
+               ps.executeUpdate();              
+           } catch (SQLException e) {
+               e.printStackTrace();              
+           }
+        }
+    }
+    
+    public List<Option> getOptionsByProductId(int productId) {
+        List<Option> optionsList = new ArrayList<>();
+        String sql = "SELECT o.option_id, o.option_name, o.price_adjustment, o.is_hidden " +
+                     "FROM ProductOptions po " +
+                     "JOIN Options o ON po.option_id = o.option_id " +
+                     "WHERE po.product_id = ?";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, productId);
+            ResultSet rs = stmt.executeQuery();
+
+            // Duyệt qua kết quả và thêm mỗi Option vào danh sách
+            while (rs.next()) {
+                Option option = new Option();
+                option.setOption_id(rs.getInt("option_id"));
+                option.setOption_name(rs.getString("option_name"));
+                option.setPrice_adjustment(rs.getDouble("price_adjustment"));
+                option.setIs_hidden(rs.getInt("is_hidden"));
+                optionsList.add(option);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return optionsList;
+    }
+    
     public boolean updateCartItem(CartItem cartItem) {
         String query = "UPDATE CartItems SET user_id = ?, product_id = ?, product_option_id = ?, quantity = ? WHERE cart_item_id = ?";
         try (Connection conn = DBConnection.getConnection();
@@ -160,5 +316,17 @@ public class CartItemDAO {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public void clearCart(int userId) {
+        String query = "DELETE FROM CartItems WHERE user_id = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setInt(1, userId);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
